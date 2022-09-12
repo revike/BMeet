d276552df4e0cd0bbbf6fe59eae51a6ca93e0128
e.murij <murij.kat@gmail.com>
2022-09-12 19:28:50 +0700

    correct asgi.py

--- docker_commands.sh
+++ docker_commands.sh
@@ -10,4 +10,4 @@ python3 manage.py migrate --fake-initial
 python3 manage.py collectstatic --noinput
 python3 manage.py create_admin
 #python3 manage.py runserver 0.0.0.0:8000
-gunicorn backend.asgi:application -k uvicorn.workers.UvicornWorker
+gunicorn backend.asgi:application -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000
