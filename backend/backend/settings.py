e3d8796579b7b75e379e6452d716f6b90918a32e
Evgeniy Fedorin <revike@ya.ru>
2022-09-13 16:56:43 +0300

    registration users

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'channels',
     'corsheaders',
 
@@ -145,11 +146,10 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination'
                                 '.LimitOffsetPagination',
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
-# Create your views here.
+from users.serializers import RegisterModelSerializer
+
+
+class RegisterApiView(generics.CreateAPIView):
+    """Регистрация пользователя"""
+    serializer_class = RegisterModelSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        if serializer.data:
+            print('send_mail')
+
+    def create(self, request, *args, **kwargs):
+        try:
+            token = self.request.headers['Authorization']
+            if Token.objects.filter(key=token.split()[1]).first():
+                return Response(data={'detail': 'You are already logged in'},
+                                status=status.HTTP_400_BAD_REQUEST)
+        except KeyError:
+            return super().create(request, *args, **kwargs)
