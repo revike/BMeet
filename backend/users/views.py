8c021f57c798b97d23c67a61e36a18bd7290ee19
Evgeniy Fedorin <revike@ya.ru>
2022-09-14 17:45:58 +0300

    update status user verify

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics
+from django.db import IntegrityError
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 from users.models import User
 from users.permissions import IsAnonymous
@@ -22,3 +25,24 @@ class RegisterApiView(RegisterUserMixin, generics.CreateAPIView,
 
 class VerificationKeyApiView(generics.UpdateAPIView):
     """Верификация пользователя"""
+    queryset = User.objects.all()
+    serializer_class = RegisterModelSerializer
+    permission_classes = (IsAnonymous,)
+    lookup_field = 'email'
+
+    def update(self, request, *args, **kwargs):
+        user = User.objects.filter(activation_key=kwargs['activation_key'],
+                                   email=kwargs['email']).first()
+        if user:
+            user.is_verify = True
+            user.activation_key = ''
+            user.save()
+            try:
+                token = Token.objects.create(user=user)
+            except IntegrityError:
+                Token.objects.get(user=user).delete()
+                token = Token.objects.create(user=user)
+            data = {'token': f'Token {token.key}'}
+            return Response(data=data, status=status.HTTP_200_OK)
+        data = {'Activation key': 'Invalid key'}
+        return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
