5a03536a53e2d017cba9af372c3317f0671cf550
e.murij <murij.kat@gmail.com>
2022-09-19 19:34:50 +0700

    nginx added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -31,7 +31,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/users/', include('users.urls', namespace='users')),
 
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0),
