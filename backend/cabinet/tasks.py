283d091f692395a2a570610ff0ca2e844297fd7d
Evgeniy Fedorin <revike@ya.ru>
2022-09-20 12:17:33 +0300

    update url tasks

--- backend/cabinet/tasks.py
+++ backend/cabinet/tasks.py
@@ -1,15 +1,13 @@
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
-from django.urls import reverse
 
 
 @shared_task(autoretry_for=(Exception,),
              retry_kwargs={'max_retries': 7, 'countdown': 10})
 def send_update_mail(email, email_old, key):
     url = settings.CORS_ALLOWED_ORIGINS[0]
-    url_mail_update = reverse('cabinet:mail_update',
-                              args=[email_old, email, key])
+    url_mail_update = f'/profile/{email_old}/{email}/{key}'
     subject = 'Подтвердите изменение email'
     msg = f'Для изменения email перейдите по ссылке:\n{url}{url_mail_update}'
     return send_mail(subject, msg, settings.EMAIL_HOST_USER,
