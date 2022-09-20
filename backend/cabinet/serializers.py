987a30e678bf741740b883564fd27a670842a65c
Evgeniy Fedorin <revike@ya.ru>
2022-09-21 01:35:21 +0300

    update serializers for documentation api

--- backend/cabinet/serializers.py
+++ backend/cabinet/serializers.py
@@ -5,6 +5,9 @@ from users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     """Сериализатор профиля пользователя"""
+    username = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
+    password = serializers.CharField(required=False)
 
     class Meta:
         model = User
