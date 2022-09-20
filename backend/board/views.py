b5c2163abaf904553344a653bc6c0f93861921d6
Evgeniy Fedorin <revike@ya.ru>
2022-09-20 20:11:48 +0300

    list boards

--- backend/board/views.py
+++ backend/board/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from board.models import Board
+from board.serializers import BoardSerializer
+
+
+class BoardListApiView(generics.ListCreateAPIView):
+    serializer_class = BoardSerializer
+    pagination_class = None
+
+    def get_queryset(self):
+        return Board.objects.filter(is_active=True, group=self.request.user)
