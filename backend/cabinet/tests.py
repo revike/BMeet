cf67634c6dc3e1fbc3d729ae940631fffe62b634
revike <revike@ya.ru>
2022-10-17 18:39:52 +0300

    tests cabinet update

--- backend/cabinet/tests.py
+++ backend/cabinet/tests.py
@@ -1,12 +1,9 @@
-import json
 from django.core.management import call_command
-from django.urls import resolve
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase, APIClient
 from users.models import User
-from cabinet.views import UpdateEmailApiView
 
 
 class TestCabinetApp(APITestCase):
@@ -15,9 +12,8 @@ class TestCabinetApp(APITestCase):
         call_command('loaddata', 'test_db.json')
         self.client = APIClient()
 
-    def logIn(self):
+    def login(self, user):
         url_login = reverse('users:login')
-        user = self.user_verify_data()
         del user['username']
         response = self.client.post(url_login, data=user)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -30,9 +26,10 @@ class TestCabinetApp(APITestCase):
 
     def test_authorized_data(self):
         """Тест получения данных из кабинета авторизованного пользователя"""
-        user_db = self.logIn()
         user = self.user_verify_data()
-        url_profile = reverse('cabinet:profile', kwargs={'username': user['username']})
+        user_db = self.login(user)
+        url_profile = reverse('cabinet:profile',
+                              kwargs={'username': user_db.username})
         response = self.client.get(url_profile)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data.get('username'), user_db.username)
@@ -40,72 +37,71 @@ class TestCabinetApp(APITestCase):
         self.assertEqual(response.data.get('password'), user_db.password)
         self.assertEqual(response.data.get('first_name'), user_db.first_name)
         self.assertEqual(response.data.get('last_name'), user_db.last_name)
-        url_profile = reverse('cabinet:profile', kwargs={'username': 'user189'})
+        url_profile = reverse('cabinet:profile',
+                              kwargs={'username': 'user189'})
         response = self.client.get(url_profile)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_unauthorized_data(self):
         """Тест получения данных из кабинета неавторизованного пользователя"""
         user = self.user_verify_data()
-        url_profile = reverse('cabinet:profile', kwargs={'username': user['username']})
+        url_profile = reverse('cabinet:profile',
+                              kwargs={'username': user['username']})
         response = self.client.get(url_profile)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_update_data(self):
         """Тест изменения данных пользователем в кабинете"""
-        user_new = self.new_user_data()
-        user_db = self.logIn()
         user = self.user_verify_data()
-        password_old = user_db.password
-        url_profile = reverse('cabinet:profile', kwargs={'username': user['username']})
-        response = self.client.get(url_profile)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        user_db = self.login(user)
+        user_new = self.new_user_data()
+        url_profile = reverse('cabinet:profile',
+                              kwargs={'username': user_db.username})
+        response_get = self.client.get(url_profile)
+        self.assertEqual(response_get.status_code, status.HTTP_200_OK)
         response = self.client.patch(url_profile, data=user_new)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        user_db = self.get_user(user['email'])
-        password_new = user_db.password
-        response = json.loads(response.content)
-        self.assertEqual(response['username'], 'IvP')
-        self.assertEqual(response['first_name'], 'Ivan')
-        self.assertEqual(response['last_name'], 'Petrov')
-        self.assertEqual(response['email'], 'user1@example.com')
-        self.assertNotEqual(password_new, password_old)
-        url_profile = reverse('cabinet:profile', kwargs={'username': user['username']})
+        self.assertNotEqual(response_get.data.get('username'),
+                            response.data.get('username'))
+        self.assertNotEqual(response_get.data.get('first_name'),
+                            response.data.get('first_name'))
+        self.assertNotEqual(response_get.data.get('last_name'),
+                            response.data.get('last_name'))
+        self.assertEqual(response_get.data.get('email'),
+                         response.data.get('email'))
+        self.assertNotEqual(response_get.data.get('password'),
+                            response.data.get('password'))
+        new_user = self.get_user(user_db.email)
+        url_profile = reverse('cabinet:profile',
+                              kwargs={'username': new_user.username})
         response = self.client.get(url_profile)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_change_email(self):
         """Тест изменения почты пользователем в кабинете"""
-        user_verify = self.user_verify_data()
-        users = self.get_user(user_verify['email'])
-        self.logIn()
-        user_verify = self.user_verify_data()
-        url_profile = reverse('cabinet:profile', kwargs={'username': user_verify['username']})
+        new_email = self.new_user_data()['email']
+        user = self.user_verify_data()
+        user_db = self.login(user)
+        url_profile = reverse('cabinet:profile',
+                              kwargs={'username': user_db.username})
         response = self.client.get(url_profile)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        response = json.loads(response.content)
-        self.assertEqual(response['email'], 'user1@example.com')
-        url_error = reverse(
-            'cabinet:mail_update',
-            kwargs={
-                'email': users.email,
-                'new_email': 'usernew@example.com',
-                'activation_key': f'{users.activation_key}e'
-            }
-        )
-        url = reverse(
-            'cabinet:mail_update',
-            kwargs={'email': users.email, 'new_email': 'usernew@example.com', 'activation_key': users.activation_key}
-        )
-        response = self.client.patch(url_error)
+        self.assertEqual(response.data.get('email'), user['email'])
+        url = reverse('cabinet:profile', kwargs={'username': user_db.username})
+        response = self.client.patch(url, data={'email': new_email})
+        email_old = response.data.get('email')
+        activation_key = self.get_user(user_db.email).activation_key
+        data_kwargs = {'email': user_db.email, 'new_email': new_email,
+                       'activation_key': f'{activation_key}e'}
+        url = reverse('cabinet:mail_update', kwargs=data_kwargs)
+        response = self.client.patch(url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(email_old, user_db.email)
+        data_kwargs['activation_key'] = activation_key
+        url = reverse('cabinet:mail_update', kwargs=data_kwargs)
         response = self.client.patch(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(resolve(url).func.view_class, UpdateEmailApiView)
-        response = json.loads(response.content)
-        self.assertEqual(response['email'], 'usernew@example.com')
-        response = self.client.get(url_profile)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(email_old, response.data.get('email'))
 
     @staticmethod
     def user_verify_data():
@@ -122,6 +118,7 @@ class TestCabinetApp(APITestCase):
         return {
             'first_name': 'Ivan',
             'last_name': 'Petrov',
+            'email': 'test@local.local',
             'username': 'IvP',
             'password': '555qwe'
         }
