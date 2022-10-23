bf217a61aac60a0e44d1e4cbc485743ae04ad7f5
revike <revike@ya.ru>
2022-10-23 21:04:01 +0300

    update text error username!

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -7,5 +7,6 @@ def username_validate(value):
     """Валидация username"""
     if not username_check(value):
         raise ValidationError(
-            'Username должен начинаться с буквы и может содержать знаки - или _ (минимальная длина 2 символа)',
+            'Username должен начинаться с буквы и может содержать'
+            ' знаки - или _ (минимальная длина 2 символа)',
             params={'value': value})
